442ea3cba8e3fe6dec815cd43892b25965acdbdf
agustin <agustin.gonzalez.10@hotmail.com>
2022-11-10 19:37:33 -0300

    primer commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <hello-world />
+  <carrito-compras />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import HelloWorld from '../components/HelloWorld'
+  import CarritoCompras from '../components/CarritoCompras'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home-view',
+    components: {
+      HelloWorld,
+      CarritoCompras
+    },
   }
-}
 </script>
